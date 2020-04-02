b28868aab4ed284f060a88851c7a08a99937851d
miquel.oliver <miquel.oliver@ligo.org>
2020-04-02 13:39:33 +0200

    update

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
-import Simulations_COVID19 as SCovid19 
+#import Simulations_COVID19 as SCovid19 
 
 path='./assets/data/'
 
